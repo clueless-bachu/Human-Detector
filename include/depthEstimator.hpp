7c92f7d0f91bbd27449b46c8e64e9d7b50a78542
Vasista <vasista1997@gmail.com>
2020-11-03 13:04:20 -0500

    [Task2.13] Update Class Definitions
    [include/depthEstimator.hpp] updated header file as requested
    [include/detTrack.hpp] updated header file as requested
    [include/ioHandler.hpp] updated header file as requested

--- include/depthEstimator.hpp
+++ include/depthEstimator.hpp
@@ -15,14 +15,12 @@ namespace vision {
 
 class DepthEstimator {
  private:
-    double humanHeight;
-    vector<double> intrinsicParams;
 
  public:
     DepthEstimator();
     ~DepthEstimator();
-    vector<double> estimateDepth(vector<vector<int>>);
-    vector<vector<double>> transform2dTo3d(vector<vector<int>>);
+    vector<double> estimateDepth(vector<vector<int>>, double, vector<double>);
+    vector<vector<double>> transform2dTo3d(vector<vector<int>>, double, vector<double>);
 };
 
 }  // namespace vision
